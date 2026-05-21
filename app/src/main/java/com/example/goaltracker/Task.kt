85a8ae18d397ef3b4885568fbd625be797c8b86f
Adrian G <adr@gml.com>
2026-05-21 12:57:05 +0300

    Change Task class to support Android API version 24

--- app/src/main/java/com/example/goaltracker/Task.kt
+++ app/src/main/java/com/example/goaltracker/Task.kt
@@ -7,14 +7,14 @@ data class Task(
     val id: String,
     val title: String,
     val description: String,
-    val startTime: LocalTime,
-    val allocatedTime: Duration,
+    val startTime: Int,
+    val allocatedTime: Int,
     val status: TaskStatus = TaskStatus.NotStarted
 )
 
 
 sealed class TaskStatus {
     object NotStarted : TaskStatus()
-    data class InProgress(val actualStartTime: LocalTime) : TaskStatus()
-    data class Completed(val completedAt: LocalTime) : TaskStatus()
+    data class InProgress(val actualStartTime: Int) : TaskStatus()
+    data class Completed(val completedAt: Int) : TaskStatus()
 }
